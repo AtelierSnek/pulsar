2c19e5d5d090e10027a23072c6a28f2bee5b7128
AtelierFox <diserasta@pm.me>
2022-10-06 09:39:59 +1100

    Add Event

---
+++ src/main/scala/pulsar/lib/Event.scala
@@ -0,0 +1,11 @@
+package lgbt.tech.tammy
+package pulsar.lib
+
+
+enum Event[I,C]:
+  case Actions(x: (I, C => C), xs: Event[I,C]) extends Event[I,C] //You ever just cons-list?
+  case Done extends Event
+
+  def hasMore: Boolean = this match
+    case Actions(_, _) => true
+    case Done => false
