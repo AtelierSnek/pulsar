f0ea62208fbed9125d8a58e8d1d155dbf9a859dd
AtelierFox <diserasta@pm.me>
2022-10-06 09:39:59 +1100

    Fix Overseer

--- src/main/scala/pulsar/world/Overseer.scala
+++ src/main/scala/pulsar/world/Overseer.scala
@@ -2,7 +2,7 @@ package lgbt.tech.tammy
 package pulsar.world
 
 
-import pulsar.lib.{ID, IDGenerator, Overseeable}
+import pulsar.lib.{ID, IDGenerator, Overseeable, Event}
 import pulsar.lib.ID.*
 import pulsar.lib.IterableExtensions.findIndex
 import pulsar.item.Item
@@ -11,27 +11,15 @@ import pulsar.creature.BodyPart
 
 enum Overseer[I: ID, C]:
   case ItemOverseer(val gen: IDGenerator[ItemID], val overseen: Seq[(ItemID,Item)],
-                    val actionQ: Seq[Event] = Seq.empty)
+                    val actionQ: Seq[Event[ItemID,Item]] = Seq.empty)
     extends Overseer[ItemID, Item]
   case BodyPartOverseer(val gen: IDGenerator[BodyPartID], val overseen: Seq[(BodyPartID, BodyPart)],
-                        val actionQ: Seq[Event] = Seq.empty)
+                        val actionQ: Seq[Event[BodyPartID,BodyPart]] = Seq.empty)
     extends Overseer[BodyPartID, BodyPart]
 
-  def overseen[I: ID, C: Overseeable]: Seq[(ItemID,Item)] | Seq[(BodyPartID,BodyPart)] = this match //Why can't this type annotation be Seq[(I,C)] ?
-    case ItemOverseer(_, o) => o
-    case BodyPartOverseer(_, o) => o
-
-  type Action = (I, C => C)
-
-  enum Event:
-    case Actions(x: Action, xs: Event) extends Event //You ever just cons-list?
-    case Done extends Event
-
-    def hasMore: Boolean = this match
-      case Actions(_, _) => true
-      case Done => false
-
-
+//  def overseen: Seq[(ItemID,Item)] | Seq[(BodyPartID,BodyPart)] = this match
+//    case ItemOverseer(_, o, _) => o
+//    case BodyPartOverseer(_, o, _) => o
 
 object Overseer {
 
--- src/main/scala/pulsar/world/World.scala
+++ src/main/scala/pulsar/world/World.scala
@@ -3,7 +3,15 @@ package pulsar.world
 
 import pulsar.item.Item
 import pulsar.world.Overseer
+import pulsar.world.Overseer.{BodyPartOverseer, ItemOverseer}
+import pulsar.lib.ID.*
+import pulsar.lib.ID.given
+import pulsar.lib.IDGenerator
 
-case object World {
-  //val world: Set[Overseer[?, ?]] = Set(ItemOverseer(), BodyPartOverseer())
+
+case class World(iO : ItemOverseer, bO : BodyPartOverseer) {
+//  private val iO = Overseer.ItemOverseer(new IDGenerator[ItemID](ItemID(0)),Seq.empty)
+//  private val bO = Overseer.BodyPartOverseer(new IDGenerator[BodyPartID](BodyPartID(0)),Seq.empty)
+  val world: Set[Overseer[?, ?]] = Set(iO, bO)
 }
+
